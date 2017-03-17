8561ef033a7ceb5dfd592d5cccf0b424c7c232d7
manukfarmers <mahhov@yahoo.com>
2017-03-16 21:05:06 -0700

    fix perp vector path

--- src/track/Path.java
+++ src/track/Path.java
@@ -6,18 +6,15 @@ import world.World;
 import java.awt.*;
 
 class Path {
-	private Point[] corner; // order: backLeft, backRight, frontLeft, frontRight
 	private Line bone;
+	private double backWidth, frontWidth;
+	Point perpendicularVector;
 	
 	Path(Point back, Point front, double backWidth, double frontWidth) {
 		bone = new Line(back, front);
-		Point perpendicularVector = bone.getPerpendicularVector();
-		corner = new Point[] {back.move(perpendicularVector, -backWidth / 2), back.move(perpendicularVector, backWidth / 2), front.move(perpendicularVector, -frontWidth / 2), front.move(perpendicularVector, frontWidth / 2)};
-	}
-	
-	Path(Point backLeft, Point backRight, Point frontLeft, Point frontRight) {
-		bone = new Line(backLeft.average(backRight), frontLeft.average(frontRight));
-		corner = new Point[] {backLeft, backRight, frontLeft, frontRight};
+		perpendicularVector = bone.getPerpendicularVector();
+		this.backWidth = backWidth;
+		this.frontWidth = frontWidth;
 	}
 	
 	double getHeight(double x, double y) {
@@ -25,8 +22,8 @@ class Path {
 	}
 	
 	void addToWorld(World world) {
-		int width = 10; // use constructor width
-		Point perpendicularVector = bone.getPerpendicularVector(); // store in class var
+		int width = 10;
+		Point perpendicularVector = bone.getPerpendicularVector();
 		for (int i = 0; i < bone.getDistance(); i += 1)
 			for (int j = -width; j < width; j += 1) {
 				Point p = bone.interpolate(i / bone.getDistance());
