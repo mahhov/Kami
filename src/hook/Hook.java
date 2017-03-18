3a5156bed00363f78c39f704abd5c461794b1e02
manukfarmers <mahhov@yahoo.com>
2017-03-17 20:42:24 -0700

    hook

--- src/hook/Hook.java
+++ src/hook/Hook.java
@@ -1,7 +1,37 @@
 package hook;
 
-/**
- * Created by manukhovanesian on 3/17/17.
- */
-public class Hook {
+import camera.TrailingCamera;
+import engine.Math3D;
+
+public class Hook implements TrailingCamera.Follow {
+	private double x, y, z;
+	private Math3D.Angle angle, angleZ;
+	
+	public Hook(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		angle = new Math3D.Angle(0);
+		angleZ = new Math3D.Angle(0);
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public double getZ() {
+		return z;
+	}
+	
+	public double getAngle() {
+		return angle.get();
+	}
+	
+	public double getAngleZ() {
+		return angleZ.get();
+	}
 }
