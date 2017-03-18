29389b8991ca6a8b50e45e447942f0dc54e375fd
manukfarmers <mahhov@yahoo.com>
2017-03-17 22:41:21 -0700

    hook tuning motion

--- src/hook/Hook.java
+++ src/hook/Hook.java
@@ -12,7 +12,7 @@ public class Hook implements WorldElement, TrailingCamera.Follow, ShapeParent {
 	private double x, y, z;
 	private double vx, vy, vz;
 	private Math3D.Angle angle, angleZ, angleTilt;
-	private double vAngleFlat, vAngleUp, vAngleTilt;
+	private double vAngleFlat;
 	public double[] norm, rightUp;
 	private long drawCounter;
 	
@@ -23,12 +23,12 @@ public class Hook implements WorldElement, TrailingCamera.Follow, ShapeParent {
 		angle = new Math3D.Angle(0);
 		angleZ = new Math3D.Angle(0);
 		angleTilt = new Math3D.Angle(0);
-		this.computeAxis();
+		computeAxis();
 	}
 	
 	private void computeAxis() {
-		this.norm = Math3D.norm(this.angle, this.angleZ, 1.);
-		this.rightUp = Math3D.axisVectorsTilt(this.norm, 1., this.angleZ, this.angleTilt);
+		norm = Math3D.norm(angle, angleZ, 1.);
+		rightUp = Math3D.axisVectorsTilt(norm, 1., angleZ, angleTilt);
 	}
 	
 	public void update(World world, Controller controller) {
@@ -39,59 +39,58 @@ public class Hook implements WorldElement, TrailingCamera.Follow, ShapeParent {
 	}
 	
 	void doControl(Controller controller) {
-		double movementMult = .01, angleMult = .01;
+		double movementMult = .03, angleMult = .008;
 		
 		if (controller.isKeyDown(Controller.KEY_W)) {
-			this.vx += this.norm[0] * movementMult;
-			this.vy += this.norm[1] * movementMult;
-			this.vz += this.norm[2] * movementMult;
+			vx += norm[0] * movementMult;
+			vy += norm[1] * movementMult;
+			vz += norm[2] * movementMult;
 		}
 		
 		if (controller.isKeyDown(Controller.KEY_S)) {
-			this.vx -= this.norm[0] * movementMult;
-			this.vy -= this.norm[1] * movementMult;
-			this.vz -= this.norm[2] * movementMult;
+			vx -= norm[0] * movementMult;
+			vy -= norm[1] * movementMult;
+			vz -= norm[2] * movementMult;
 		}
 		
 		if (controller.isKeyDown(Controller.KEY_A)) {
-			this.vAngleTilt += angleMult;
+			vx -= rightUp[0] * movementMult;
+			vy -= rightUp[1] * movementMult;
+			vz -= rightUp[2] * movementMult;
 		}
 		
 		if (controller.isKeyDown(Controller.KEY_D)) {
-			this.vAngleTilt -= angleMult;
+			vx += rightUp[0] * movementMult;
+			vy += rightUp[1] * movementMult;
+			vz += rightUp[2] * movementMult;
 		}
 		
 		if (controller.isKeyDown(Controller.KEY_SPACE)) {
-			this.vAngleUp += angleMult;
-		}
-		
-		if (controller.isKeyDown(Controller.KEY_SHIFT)) {
-			this.vAngleUp -= angleMult;
+			vx += rightUp[3] * movementMult;
+			vy += rightUp[4] * movementMult;
+			vz += rightUp[5] * movementMult;
 		}
 		
 		if (controller.isKeyDown(Controller.KEY_Q)) {
-			this.vAngleFlat += angleMult;
+			vAngleFlat += angleMult;
 		}
 		
-		if (controller.isKeyDown(Controller.KEY_R)) {
-			this.vAngleFlat -= angleMult;
+		if (controller.isKeyDown(Controller.KEY_E)) {
+			vAngleFlat -= angleMult;
 		}
 	}
 	
 	private void move(World world) {
 		int safeZone = 2;
-		double friction = .95, gravity = -.005;
+		double friction = .9, gravity = -.01;
 		vx *= friction;
 		vy *= friction;
 		vz = (vz + gravity) * friction;
 		vAngleFlat *= friction;
-		vAngleUp *= friction;
-		vAngleTilt *= friction;
 		x += vx;
 		y += vy;
 		z += vz;
-		Math3D.Angle.rotate(angle, angleZ, angleTilt, rightUp, vAngleFlat, vAngleUp);
-		angleTilt.set(angleTilt.get() + vAngleTilt);
+		angle.set(angle.get() + vAngleFlat);
 		computeAxis();
 		double[] xyz = Math3D.bound(x - safeZone, y - safeZone, z - safeZone, world.width - safeZone * 2, world.length - safeZone * 2, world.height - safeZone * 2);
 		x = xyz[0] + safeZone;
@@ -100,7 +99,7 @@ public class Hook implements WorldElement, TrailingCamera.Follow, ShapeParent {
 	}
 	
 	void addToWorld(World world) {
-		Cube shape = new Cube(x, y, z, angle, angleZ, angleTilt, .5, this);
+		Cube shape = new Cube(x + .5, y + .5, z + .5, angle, angleZ, angleTilt, .5, this);
 		world.addShape((int) x, (int) y, (int) z, shape);
 	}
 	
