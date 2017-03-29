157dd64e76180f262d5e3850b0648cec7ce7f7c1
manukfarmers <mahhov@yahoo.com>
2017-03-29 07:29:15 -0700

    smoke particle tweak

--- src/particle/SmokeParticle.java
+++ src/particle/SmokeParticle.java
@@ -5,7 +5,7 @@ import engine.Math3D;
 import java.awt.*;
 
 public class SmokeParticle extends Particle {
-	private final static double VELOCITY_SPREAD = .03, VZ_OFFSET = -.3;
+	private final static double VELOCITY_SPREAD = .03, VZ_OFFSET = -.03;
 	
 	public SmokeParticle(double x, double y, double z) {
 		super(x, y, z, .1, Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), VZ_OFFSET + Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), Color.WHITE, 300);
