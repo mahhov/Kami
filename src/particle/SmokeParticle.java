d0608eb7f8298e485a23595bbba3068de869bda5
manukfarmers <mahhov@yahoo.com>
2017-03-31 15:59:41 -0700

    smoke spread

--- src/particle/SmokeParticle.java
+++ src/particle/SmokeParticle.java
@@ -5,9 +5,9 @@ import engine.Math3D;
 import java.awt.*;
 
 public class SmokeParticle extends Particle {
-	private final static double VELOCITY_SPREAD = .03, VZ_OFFSET = -.03;
+	private final static double VELOCITY_SPREAD = .03, VZ_OFFSET = -.03, POS_SPREAD = .3;
 	
 	public SmokeParticle(double x, double y, double z) {
-		super(x, y, z, .1, Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), VZ_OFFSET + Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), Color.WHITE, 600);
+		super(x + Math3D.rand(-POS_SPREAD, POS_SPREAD), y + Math3D.rand(-POS_SPREAD, POS_SPREAD), z + Math3D.rand(-POS_SPREAD, POS_SPREAD), .1, Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), VZ_OFFSET + Math3D.rand(-VELOCITY_SPREAD, VELOCITY_SPREAD), Color.WHITE, 600);
 	}
 }
