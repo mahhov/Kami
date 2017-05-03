09e82b13c525dadff8e72254d9a24ae9c0ffd446
manukfarmers <mahhov@yahoo.com>
2017-05-03 08:21:49 -0700

    git ignore timelog

--- src/engine/Timer.java
+++ src/engine/Timer.java
@@ -4,23 +4,26 @@ import java.io.PrintWriter;
 
 public class Timer {
 	public static final Time WORLD_CONSTRUCTOR = new Time("World Constructor", 0, true);
+	public static final Time TERRAIN_CONSTRUCTOR = new Time("Terrain Constructor", 0, true);
+	
 	public static final Time BACKGROUND = new Time("Background", 10, true);
 	public static final Time CHUNKS = new Time("Chunks", 70, true);
 	public static final Time INTERFACE = new Time("Interface", 10, true);
 	public static final Time CELL_DRAW_AGGREGATED = new Time("Cell.draw aggregated", 40, true);
 	public static final Time TO_CAMERA_AGGREGATED = new Time("toCamera aggregated", 20, true);
+	
 	public static final Time PAINTER_QUEUE_PAINT = new Time("painterQueue.paint()", 10, true);
 	public static final Time PAINT = new Time("paint()", 10, true);
 	public static final Time LOOP_1 = new Time("loop 1", 20, true);
 	public static final Time WORLD_DRAW = new Time("world.draw", 100, true);
 	public static final Time FIND = new Time("find", 100, true);
-	public static final Time TERRAIN_CONSTRUCTOR = new Time("Terrain Constructor", 0, true);
 	public static final Time EXPAND = new Time("expand", 20, true);
 	public static final Time ADD_TO_WORLD = new Time("add to world", 10, true);
 	
 	static void writeFile() {
 		String s = "";
 		s += WORLD_CONSTRUCTOR.toFileString();
+		s += TERRAIN_CONSTRUCTOR.toFileString();
 		s += BACKGROUND.toFileString();
 		s += CHUNKS.toFileString();
 		s += INTERFACE.toFileString();
@@ -31,7 +34,6 @@ public class Timer {
 		s += LOOP_1.toFileString();
 		s += WORLD_DRAW.toFileString();
 		s += FIND.toFileString();
-		s += TERRAIN_CONSTRUCTOR.toFileString();
 		s += EXPAND.toFileString();
 		s += ADD_TO_WORLD.toFileString();
 		
