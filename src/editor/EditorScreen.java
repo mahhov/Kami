6dbf7def7ea807fc0714f0f490847bfa7c70a28a
manukfarmers <mahhov@yahoo.com>
2017-06-18 17:55:37 -0700

    return up scroll button, create load/save button ui

--- src/editor/EditorScreen.java
+++ src/editor/EditorScreen.java
@@ -18,14 +18,8 @@ public class EditorScreen {
 	public EditorScreen(double left, double top, double width, double height) {
 		screenEditorMap = new ScreenEditorMap(MAP_WIDTH, MAP_LENGTH, MAP_HEIGHT);
 		
-		cell = new ScreenCell(.02, 20, 20);
+		cell = new ScreenCell(.02, 21, 21);
 		cell.setPosition(left, top, width, height);
-		
-		// draw, alpha, zoom
-		cell.addScreenItem(drawButton = new ScreenButton("DRAW", ' '), 9, 0, 2, 1);
-		cell.addScreenItem(alphaButton = new ScreenToggleButton("ALPHA", 't'), 11, 0, 2, 1);
-		cell.addScreenItem(zoomOutButton = new ScreenButton("ZOOM OUT", '-'), 13, 0, 3, 1);
-		cell.addScreenItem(zoomInButton = new ScreenButton("ZOOM IN", '='), 16, 0, 3, 1);
 		
 		// trigger
 		triggerGroup = new SelectButtonGroup();
@@ -37,6 +31,12 @@ public class EditorScreen {
 		cell.addScreenItem(triggerGroup.add(new ScreenSelectButton("T 6")), 5, 0, 1, 1);
 		cell.addScreenItem(triggerGroup.add(new ScreenSelectButton("T 7")), 6, 0, 1, 1);
 		cell.addScreenItem(triggerGroup.add(new ScreenSelectButton("T 8")), 7, 0, 1, 1);
+		
+		// draw, alpha, zoom
+		cell.addScreenItem(drawButton = new ScreenButton("DRAW", ' '), 9, 0, 2, 1);
+		cell.addScreenItem(alphaButton = new ScreenToggleButton("ALPHA", 't'), 11, 0, 2, 1);
+		cell.addScreenItem(zoomOutButton = new ScreenButton("ZOOM OUT", '-'), 13, 0, 3, 1);
+		cell.addScreenItem(zoomInButton = new ScreenButton("ZOOM IN", '='), 16, 0, 3, 1);
 		
 		// block type
 		toolGroup = new SelectButtonGroup();
@@ -50,25 +50,28 @@ public class EditorScreen {
 		toolGroup.setSelect(1);
 		
 		// map, vert, scroll, minimap
-		cell.addScreenItem(vertMapTable = new ScreenTable(1, MAP_HEIGHT), 0, 2, 2, 16);
-		upButton = new ScreenButton("UP", 'w');
-		//		cell.addScreenItem(upButton = new ScreenButton("UP", 'w'), 3, 1, 16, 1);
-		cell.addScreenItem(downButton = new ScreenButton("DOWN", 's'), 3, 18, 16, 1);
-		cell.addScreenItem(leftButton = new ScreenButton("L", 'a'), 2, 2, 1, 16);
-		cell.addScreenItem(rightButton = new ScreenButton("R", 'd'), 19, 2, 1, 16);
-		cell.addScreenItem(new ScreenImageContainer(screenEditorMap), 14, 13, 4, 4);
-		cell.addScreenItem(screenEditorMap, 3, 2, 16, 16);
+		cell.addScreenItem(vertMapTable = new ScreenTable(1, MAP_HEIGHT), 0, 3, 2, 16);
+		cell.addScreenItem(upButton = new ScreenButton("UP", 'w'), 3, 2, 16, 1);
+		cell.addScreenItem(downButton = new ScreenButton("DOWN", 's'), 3, 19, 16, 1);
+		cell.addScreenItem(leftButton = new ScreenButton("L", 'a'), 2, 3, 1, 16);
+		cell.addScreenItem(rightButton = new ScreenButton("R", 'd'), 19, 3, 1, 16);
+		cell.addScreenItem(new ScreenImageContainer(screenEditorMap), 14, 14, 4, 4);
+		cell.addScreenItem(screenEditorMap, 3, 3, 16, 16);
+		
+		// load, save
+		cell.addScreenItem(drawButton = new ScreenButton("LOAD", 'L'), 20, 3, 1, 1);
+		cell.addScreenItem(drawButton = new ScreenButton("SAVE", 'S'), 20, 4, 1, 1);
 		
 		// selection
-		cell.addScreenItem(clearAllSelectionButton = new ScreenButton("CLEAR ALL SELECTION", 'c'), 0, 19, 4, 1);
-		cell.addScreenItem(clearSelectionButton = new ScreenButton("CLEAR SELECTION", 'z'), 4, 19, 4, 1);
-		cell.addScreenItem(unselectModeButton = new ScreenToggleButton("UNSELECT MODE", 'x'), 8, 19, 4, 1);
+		cell.addScreenItem(clearAllSelectionButton = new ScreenButton("CLEAR ALL SELECTION", 'c'), 0, 20, 4, 1);
+		cell.addScreenItem(clearSelectionButton = new ScreenButton("CLEAR SELECTION", 'z'), 4, 20, 4, 1);
+		cell.addScreenItem(unselectModeButton = new ScreenToggleButton("UNSELECT MODE", 'x'), 8, 20, 4, 1);
 		
 		// draw mode
 		drawGroup = new SelectButtonGroup();
-		cell.addScreenItem(drawGroup.add(new ScreenSelectButton("PEN", 'b')), 14, 19, 2, 1);
-		cell.addScreenItem(drawGroup.add(new ScreenSelectButton("LINE", 'n')), 16, 19, 2, 1);
-		cell.addScreenItem(drawGroup.add(new ScreenSelectButton("RECT", 'm')), 18, 19, 2, 1);
+		cell.addScreenItem(drawGroup.add(new ScreenSelectButton("PEN", 'b')), 14, 20, 2, 1);
+		cell.addScreenItem(drawGroup.add(new ScreenSelectButton("LINE", 'n')), 16, 20, 2, 1);
+		cell.addScreenItem(drawGroup.add(new ScreenSelectButton("RECT", 'm')), 18, 20, 2, 1);
 	}
 	
 	public void update(InputControllerJava controller) {
